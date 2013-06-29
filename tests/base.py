eee06733a2ec30c5ba69e78b003138e9b1265740
Max Countryman <max.countryman@gmail.com>
2013-06-28 19:06:25 -0700

    ensuring unittest2 is used

--- tests/base.py
+++ tests/base.py
@@ -1,5 +1,12 @@
 import unittest
 
+if not hasattr(unittest.TestCase, 'assertIn'):
+    try:
+        import unittest2 as unittest
+    except:
+        print('unittest2 is required to run this test suite!')
+        raise
+
 
 class SimpleIrcTestCase(unittest.TestCase):
     pass
